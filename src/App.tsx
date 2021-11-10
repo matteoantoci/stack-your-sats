4d3eacd28366a38a026cfb802ad7077a6af5fe45
Matteo Antoci <matteo.antoci@lastminute.com>
2021-11-10 12:29:14 +0100

    Many improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
-import './App.css'
 import { Chart } from './components/Chart'
 import { ThemeProvider } from '@mui/material/styles'
 import { theme } from './theme'
-import { Container } from '@mui/material'
+import { Box, Container, CssBaseline, Stack, styled, Typography } from '@mui/material'
+
+const Main = styled('main')`
+  padding-top: ${theme.spacing(3)};
+  padding-bottom: ${theme.spacing(3)};
+`
 
 const App = () => (
   <ThemeProvider theme={theme}>
-    <main>
+    <CssBaseline />
+    <Main>
       <Container>
-        <Chart />
+        <Stack spacing={3}>
+          <Box>
+            <Typography variant={'h2'} component={'h1'}>
+              Stack your sats!
+            </Typography>
+          </Box>
+          <Chart />
+        </Stack>
       </Container>
-    </main>
+    </Main>
   </ThemeProvider>
 )
 
--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,7 +1,9 @@
-import { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { Bar, BarChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 import { theme } from '../theme'
-import { Paper, Stack, styled, Typography } from '@mui/material'
+import { Box, FormControl, MenuItem, Paper, Stack, styled, TextField, Typography } from '@mui/material'
+import Big from 'big.js'
+import numeral from 'numeral'
 
 const FONT_SIZE = theme.typography.fontSize
 
@@ -9,50 +11,188 @@ export const getIntSpacing = (spacing: number): number => parseInt(theme.spacing
 
 type ChartData = {
   year: string
-  interests: number
-  contributions: number
-  total: number
+  interests: Big
+  contributions: Big
+  total: Big
 }
 
-const chartData: ChartData[] = [
-  { year: '2021', interests: 0, contributions: 10000, total: 10000 },
-  { year: '2022', interests: 1000, contributions: 20000, total: 21000 },
-  { year: '2023', interests: 3000, contributions: 30000, total: 33000 },
-]
+type ChartArgs = {
+  yearsToGrow: number
+  startingAmount: number
+  monthlyContribution: number
+  rate: number
+}
 
-export const createCurrencyFormatter = (currency: string) => (value: number) =>
-  new Intl.NumberFormat(navigator.language, {
-    style: 'currency',
-    currency,
-  }).format(value)
+const createChartData = (args: ChartArgs) => {
+  const currentYear = new Date().getFullYear()
+  const startingAmount = new Big(args.startingAmount)
+  const yearlyContribution = new Big(args.monthlyContribution).mul(12)
+  const monthlyRate = new Big(args.rate).div(100).plus(1)
+
+  const chartData: ChartData[] = Array(args.yearsToGrow)
+    .fill(undefined)
+    .reduce((chartDataAcc: ChartData[], _, year): ChartData[] => {
+      const prev = chartDataAcc[year - 1]
+      const prevTotal = prev ? prev.total : startingAmount
+      const contributions = startingAmount.plus(yearlyContribution.mul(year + 1))
+
+      const total = prevTotal.mul(monthlyRate).plus(yearlyContribution).round()
+
+      return [
+        ...chartDataAcc,
+        {
+          year: (currentYear + year).toString(),
+          contributions,
+          interests: total.minus(contributions),
+          total,
+        },
+      ]
+    }, [] as ChartData[])
+
+  chartData.unshift({
+    year: 'Start',
+    contributions: startingAmount,
+    interests: Big(0),
+    total: startingAmount,
+  })
+
+  return chartData
+}
 
 const Card = styled(Paper)`
   padding: ${theme.spacing(3)};
   height: 100%;
 `
 
-export const Chart: FC = () => (
-  <Card>
-    <Stack spacing={3}>
-      <Typography variant={'h2'}>Stack your sats!</Typography>
-      <ResponsiveContainer height={getIntSpacing(50)}>
-        <BarChart data={chartData}>
-          <CartesianGrid opacity={0.3} />
-          <XAxis dataKey="year" tick={{ fontSize: FONT_SIZE }} />
-          <YAxis tick={{ fontSize: FONT_SIZE }} />
-          <Tooltip
-            formatter={createCurrencyFormatter('EUR')}
-            contentStyle={{
-              color: theme.palette.text.primary,
-              backgroundColor: theme.palette.background.default,
-              fontSize: FONT_SIZE,
-              borderRadius: `${theme.shape.borderRadius}px`,
-            }}
-          />
-          <Bar dataKey="contributions" stackId="a" fill={theme.palette.success.main} name="Contributions" />
-          <Bar dataKey="interests" stackId="a" fill={theme.palette.primary.main} name="Interests" />
-        </BarChart>
-      </ResponsiveContainer>
-    </Stack>
-  </Card>
-)
+type Currency = {
+  readonly value: string
+  readonly label: string
+}
+
+const CURRENCIES = [
+  {
+    value: 'USD',
+    label: '$',
+  },
+  {
+    value: 'EUR',
+    label: '€',
+  },
+  {
+    value: 'GBP',
+    label: '£',
+  },
+] as const
+
+const formatAxisValue = (value: number) => {
+  const formatted = numeral(value).format(`(0a)`)
+  return value > 1000000 ? formatted.toUpperCase() : formatted
+}
+
+const toInt = (value: string) => {
+  return value && value.length ? parseInt(value) : 0
+}
+
+export const Chart: FC = () => {
+  const [currency, setCurrency] = useState<Currency>(CURRENCIES[1])
+  const [yearsToGrow, setYearsToGrow] = useState(5)
+  const [startingAmount, setStartingAmount] = useState(1000)
+  const [monthlyContribution, setMonthlyContribution] = useState(100)
+  const [rate, setRate] = useState(162)
+
+  const data = createChartData({
+    yearsToGrow,
+    startingAmount,
+    monthlyContribution,
+    rate,
+  })
+
+  return (
+    <Card>
+      <Stack spacing={3}>
+        <Typography variant={'h6'} component={'h2'} color={theme.palette.text.secondary}>
+          See how your money will grow over time with Bitcoin compound annual growth rate (CAGR).
+        </Typography>
+        <FormControl>
+          <Stack direction="row" justifyContent="space-between" spacing={1}>
+            <TextField
+              select
+              label="Currency"
+              value={currency.value}
+              size="small"
+              onChange={(e) => {
+                const selectedIndex = CURRENCIES.findIndex((it) => it.value === e.target.value) ?? 0
+                setCurrency(CURRENCIES[selectedIndex])
+              }}
+            >
+              {CURRENCIES.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  <Stack direction="row" spacing={0.5}>
+                    <Box component={'span'}>{option.label}</Box>
+                    <Box component={'span'} color={theme.palette.text.secondary}>
+                      {option.value}
+                    </Box>
+                  </Stack>
+                </MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              label="Initial balance"
+              value={startingAmount}
+              type="number"
+              size="small"
+              onChange={(e) => setStartingAmount(toInt(e.target.value))}
+            />
+            <TextField
+              label="Monthly contribution"
+              type="number"
+              value={monthlyContribution}
+              size="small"
+              onChange={(e) => setMonthlyContribution(toInt(e.target.value))}
+            />
+            <TextField
+              label="Est. Bitcoin CAGR"
+              type="number"
+              defaultValue={rate}
+              size="small"
+              onChange={(e) => setRate(toInt(e.target.value))}
+            />
+            <TextField
+              label="Years"
+              type="number"
+              value={yearsToGrow}
+              size="small"
+              onChange={(e) => setYearsToGrow(toInt(e.target.value))}
+            />
+          </Stack>
+        </FormControl>
+
+        <ResponsiveContainer height={getIntSpacing(80)}>
+          <BarChart data={data}>
+            <CartesianGrid opacity={0.3} />
+            <XAxis dataKey="year" tick={{ fontSize: FONT_SIZE }} />
+            <YAxis tickFormatter={formatAxisValue} tick={{ fontSize: FONT_SIZE }} />
+            <Tooltip
+              formatter={(value: number) =>
+                new Intl.NumberFormat(navigator.language, {
+                  style: 'currency',
+                  currency: currency.value,
+                  maximumFractionDigits: 0,
+                  currencyDisplay: 'narrowSymbol',
+                }).format(value)
+              }
+              contentStyle={{
+                color: theme.palette.text.primary,
+                backgroundColor: theme.palette.background.default,
+                fontSize: FONT_SIZE,
+                borderRadius: `${theme.shape.borderRadius}px`,
+              }}
+            />
+            <Bar dataKey="contributions" stackId="a" fill={theme.palette.success.main} name="Contributions" />
+            <Bar dataKey="interests" stackId="a" fill={theme.palette.primary.main} name="Interests" />
+          </BarChart>
+        </ResponsiveContainer>
+      </Stack>
+    </Card>
+  )
+}
